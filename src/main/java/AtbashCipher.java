88536ffdee8f7101318487ad3032ac485db02e91
Maksym <maksym.kozachuk@protonmail.com>
2020-03-26 14:38:48 +0100

    AtbashCipher

--- src/main/java/AtbashCipher.java
+++ src/main/java/AtbashCipher.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,22 +11,43 @@ public class AtbashCipher implements Cryptor {
 
     @Override
     public String crypt(String userInputForCrypt) {
+        cryptedMessage = new ArrayList<String>();
+        char[] userInputChars = userInputForCrypt.toCharArray();
+        for(int i = 0; i <= userInputForCrypt.length() - 1; i++){
+            cryptedMessage.add(String.valueOf(userInputChars[i]));
+            if(!cryptedMessage.get(i).equals(" ")) {
+                int ind = Arrays.asList(alphabet).indexOf(cryptedMessage.get(i));
+                cryptedMessage.set(i, cryptoAlphabet[ind]);
+            }
+        }
 
-        return null;
+        for(String letters : cryptedMessage) {
+            crypted += letters;
+        }
+
+        return crypted;
     }
 
     @Override
     public void printCryptedResult() {
-
+        if(!crypted.isEmpty() && !crypted.equals("")) {
+            System.out.println(crypted);
+        }
     }
 
     @Override
     public void createNewCryptAlphabet(String alphabetDependency) {
 
+        for( int i = 0; i < alphabet.length; i++){
+            cryptoAlphabet[i] = alphabet[alphabet.length - i - 1];
+        }
+        System.out.println(Arrays.toString(cryptoAlphabet));
     }
 
     @Override
     public void printAlphabet() {
-
+        for(int i = 0; i < alphabet.length; i++) {
+            System.out.println(alphabet[i] + " -> " + cryptoAlphabet[i]);
+        }
     }
 }
