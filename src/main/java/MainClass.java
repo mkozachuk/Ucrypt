11fcb1d92584e605aeaf74748694d55e1db0a5f9
Maksym <maksym.kozachuk@protonmail.com>
2020-03-29 18:05:32 +0200

    VigenereCipher test

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,11 +1,42 @@
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
-    Cryptor cryptor = new MorseCode();
+    VigenereCipher cryptor = new VigenereCipher();
 //        cryptor.createNewCryptAlphabet("1");
 ////        cryptor.crypt("HELLO WORLD");
 ////        cryptor.printCryptedResult();
-        cryptor.printAlphabet();
-        cryptor.crypt("SOS");
-        cryptor.printCryptedResult();
+//
+//        ((VigenereCipher) cryptor).createTabulaRecta();
+//        cryptor.crypt("dawdad1 2dawda");
+        cryptor.createTabulaRecta();
+        String[][] tabulaRecta = cryptor.getTabulaRecta();
+        int userInputLength = 10;
+        char userInputChar;
+        char secretKeyChar;
+        int u;
+        int s;
+        String userInputForCrypt = "ATTACKATDA";
+        char[] longKey = new char[10];
+        String longKeyString = "LEMONLEMON";
+        for(int i = 0; i < longKey.length; i++){
+            longKey[i] = longKeyString.charAt(i);
+        }
+        String encryptedChat = "";
+
+
+
+        for (int i = 0; i < userInputLength; i++){
+            userInputChar = userInputForCrypt.charAt(i);
+            secretKeyChar = longKey[i];
+            u = Arrays.asList(tabulaRecta[0]).indexOf(String.valueOf(userInputChar));
+            s = Arrays.asList(tabulaRecta[0]).indexOf(String.valueOf(secretKeyChar));
+            encryptedChat = tabulaRecta[u][s];
+//            System.out.println(" U " + u);
+//            System.out.println(" S " + s);
+
+            System.out.print(encryptedChat);
+        }
+        System.out.print(encryptedChat);
     }
 }
--- src/main/java/VigenereCipher.java
+++ src/main/java/VigenereCipher.java
@@ -7,12 +7,50 @@ public class VigenereCipher implements Cryptor {
     private String crypted = "";
     private String[][] tabulaRecta;
     private String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
             "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private int uniqueAlphabetLetters = 26;
     private String[] cryptoAlphabet = new String[26];
-    private List<String> cryptedMessage;
+    //private List<String> cryptedMessage;
+    private char[] encryptedMessage;
 
     @Override
     public String crypt(String userInputForCrypt) {
+        String correctUserInput = userInputForCrypt.toUpperCase().replaceAll(" ", "");
+        int userInputLength = correctUserInput.length();
+        int secretKeyLength = getSecretKey().length();
+        char[] longKey = new char[userInputLength];
+        StringBuilder longKeyString = new StringBuilder();
+        encryptedMessage = new char[userInputLength];
+
+        char userInputChar;
+        char secretKeyChar;
+        String encryptedChat;
+
+        int u;
+        int s;
+        int e;
+
+
+        for (int i = 0; i < userInputLength/secretKeyLength + 1; i++ ) {
+           longKeyString.append(getSecretKey());
+        }
+
+        for(int i = 0; i < longKey.length; i++){
+            longKey[i] = longKeyString.charAt(i);
+        }
+
+        for (int i = 0; i < userInputLength; i++){
+            userInputChar = userInputForCrypt.charAt(i);
+            secretKeyChar = longKey[i];
+            u = Arrays.asList(tabulaRecta).indexOf(userInputChar);
+            s = Arrays.asList(tabulaRecta).indexOf(secretKeyChar);
+            encryptedChat = tabulaRecta[u][s];
+        }
+
+
+        System.out.println(correctUserInput);
         return null;
     }
 
@@ -26,35 +64,34 @@ public class VigenereCipher implements Cryptor {
 
     }
 
+
     @Override
     public void printAlphabet() {
-
-    }
-
-    public void createTabulaRecta(){
-        tabulaRecta = new String[26][26];
-        for (int i = 0; i < alphabet.length; i++){
-            for (int j = 0; j < tabulaRecta[i].length; j++){
-                tabulaRecta[i][j] = alphabet[j];
-            }
-        }
-
+        System.out.println("Tabula Recta: ");
         for(int i = 0; i < tabulaRecta.length; i++) {
             System.out.println(Arrays.toString(tabulaRecta[i]));
         }
     }
 
-    public static int[] shiftLeft(int[] a, int shift) {
-        if (a != null) {
-            int length = a.length;
-            int[] b = new int[length];
-            // шаг 1
-            System.arraycopy(a, shift, b, 0, length - shift);
-            // шаг 2
-            System.arraycopy(a, 0, b, length - shift, shift);
-            return b;
-        } else {
-            return null;
+    public void createTabulaRecta(){
+        tabulaRecta = new String[26][26];
+        for (int i = 0; i < uniqueAlphabetLetters; i++){
+            for (int j = 0; j < uniqueAlphabetLetters; j++){
+                tabulaRecta[i][j] = alphabet[j + i];
+            }
         }
+
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public String[][] getTabulaRecta() {
+        return tabulaRecta;
     }
 }
