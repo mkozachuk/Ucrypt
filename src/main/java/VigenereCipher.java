c7ca405052829109d6337b3a3f5ec02dc6249490
Maksym <maksym.kozachuk@protonmail.com>
2020-03-29 18:13:46 +0200

    VigenereCipher test1

--- src/main/java/VigenereCipher.java
+++ src/main/java/VigenereCipher.java
@@ -12,7 +12,6 @@ public class VigenereCipher implements Cryptor {
             "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     private int uniqueAlphabetLetters = 26;
     private String[] cryptoAlphabet = new String[26];
-    //private List<String> cryptedMessage;
     private char[] encryptedMessage;
 
     @Override
@@ -26,11 +25,11 @@ public class VigenereCipher implements Cryptor {
 
         char userInputChar;
         char secretKeyChar;
-        String encryptedChat;
+        String encryptedChar;
 
         int u;
         int s;
-        int e;
+
 
 
         for (int i = 0; i < userInputLength/secretKeyLength + 1; i++ ) {
@@ -44,19 +43,19 @@ public class VigenereCipher implements Cryptor {
         for (int i = 0; i < userInputLength; i++){
             userInputChar = userInputForCrypt.charAt(i);
             secretKeyChar = longKey[i];
-            u = Arrays.asList(tabulaRecta).indexOf(userInputChar);
-            s = Arrays.asList(tabulaRecta).indexOf(secretKeyChar);
-            encryptedChat = tabulaRecta[u][s];
+            u = Arrays.asList(tabulaRecta[0]).indexOf(String.valueOf(userInputChar));
+            s = Arrays.asList(tabulaRecta[0]).indexOf(String.valueOf(secretKeyChar));
+            encryptedChar = tabulaRecta[u][s];
+            crypted += encryptedChar;
         }
-
-
-        System.out.println(correctUserInput);
-        return null;
+        return crypted;
     }
 
     @Override
     public void printCryptedResult() {
-
+        if(!crypted.isEmpty() && !crypted.equals("")) {
+            System.out.println(crypted);
+        }
     }
 
     @Override
