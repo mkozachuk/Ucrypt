7c28581692f78768f92d7ad333054a6b64a71ee3
Maksym <maksym.kozachuk@protonmail.com>
2020-04-08 10:16:36 +0200

    Ceasar Decryptor

---
+++ src/main/java/decryptors/A1Z26Dec.java
@@ -0,0 +1,14 @@
+package decryptors;
+
+public class A1Z26Dec implements Decryptor {
+
+    @Override
+    public String decrypt(String userInputForDecrypt) {
+        return null;
+    }
+
+    @Override
+    public void printDecryptedResult() {
+
+    }
+}
