8f5217f2c5c88a552dc82730c10c49f5a00f1bdf
Maksym <maksym.kozachuk@protonmail.com>
2020-04-08 10:51:21 +0200

    A1Z26Dec

--- src/main/java/decryptors/A1Z26Dec.java
+++ src/main/java/decryptors/A1Z26Dec.java
@@ -1,10 +1,45 @@
 package decryptors;
 
+import encryptors.A1Z26Cipher;
+import encryptors.CaesarCipher;
+
+import javax.crypto.Cipher;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class A1Z26Dec implements Decryptor {
+
+    private A1Z26Cipher cipher = new A1Z26Cipher();
+
+    private List<String> decryptedMessage;
+
+    public String decrypted ="";
+
+    private String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+
+    private int [] cryptoAlphabet = cipher.getCryptoAlphabet();
 
     @Override
     public String decrypt(String userInputForDecrypt) {
-        return null;
+        decryptedMessage = new ArrayList<String>();
+        char[] userInputChars = userInputForDecrypt.toCharArray();
+        cipher.createNewCryptAlphabet();
+        for(int i = 0; i <userInputForDecrypt.length(); i++){
+            decryptedMessage.add(String.valueOf(userInputChars[i]));
+            if(!decryptedMessage.get(i).equals(" ")) {
+                int currentIntIndex = Integer.parseInt(decryptedMessage.get(i)) - 1;
+                decryptedMessage.set(i, alphabet[currentIntIndex]);
+            }
+        }
+
+        for(String letters : decryptedMessage) {
+            decrypted += letters;
+
+        }
+        return decrypted;
     }
 
     @Override
